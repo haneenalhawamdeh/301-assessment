c39e37f4136425375c7ff46e2bc6cb01d6c749af
haneen <your email here in single quotes>
2019-11-20 17:59:36 +0200

    add

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+require('dotenv').config();
+const cors = require('cors')
+const PORT = process.env.PORT
+server.use(cors());
+
+get ('/',(Request,Response)=>
+Response.json(a) 
+
+)
