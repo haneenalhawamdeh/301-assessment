76cf19adf64a1b599e58ee498b573203c614b0da
haneen <your email here in single quotes>
2019-11-20 18:35:01 +0200

    add

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const PORT = process.env.PORT
 server.use(cors());
 
 get ('/',(Request,Response)=>
-Response.json(a) 
+Response.json('./data/a') 
 
 )
+// should we use object.entries to let the output like 
+for (let [key, value] of Object.entries(a)) 
+
+const Person(){
+this.name= name,
+this.pets=["rosie","rocky","luna"]
+
+ 
+}
+//should use filter with cond start with r 
+
+Person.prototype.age
